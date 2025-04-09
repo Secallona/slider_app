83d1bc17df4467157b0e84a1b33b01d52f536bc8
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-04-09 19:22:49 +0200

    feat: Implement slider functionality and update UI in ContentView

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,63 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: Scaffold(body: ContentView()));
+  }
+}
+
+class ContentView extends StatefulWidget {
+  const ContentView({super.key});
+
+  @override
+  State<ContentView> createState() => _ContentViewState();
+}
+
+class _ContentViewState extends State<ContentView> {
+  double _value = 50.0;
+  final double _min = 1.0;
+  final double _max = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("💀💀💀💀"),
+          Text("68"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("${_min.toInt()}"),
+                Expanded(
+                  child: Slider(
+                    value: _value,
+                    onChanged: _onChanged,
+                    min: _min,
+                    max: _max,
+                  ),
+                ),
+                Text("${_max.toInt()}"),
+              ],
+            ),
+          ),
+          Text("${_value.toInt()}"),
+          ElevatedButton(onPressed: onPressed, child: Text("TRY")),
+        ],
       ),
     );
+  }
+
+  void _onChanged(double value) {
+    setState(() {
+      _value = value;
+      //this._value = value;
+    });
+  }
+
+  void onPressed() {
+    print("Botón pulsado");
   }
 }
