3eeb5a9b79c7a3341194de00586b37b73af525c3
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-05-03 16:13:11 +0200

    feat: Add marks tracking to the Game class for score and rounds

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -8,12 +8,16 @@ class Game {
   int _points = 0;
   int _score = 0;
   int _rounds = 0;
+  //Queremos guardar las marcas en una lista de listas, donde cada lista contiene el score y el round.
+  //Para ello, utilizamos List<List<int>>. La primera lista contiene el score y la segunda el round.
+  List<List<int>> _marks = [];
 
 
   int get targetValue => _targetValue;
   int get points => _points;
   int get score => _score;
   int get rounds => _rounds;
+  List<List<dynamic>> get marks => _marks;
 
   Game() {
     _targetValue = Random().nextInt(MAX_VALUE + 1 - MIN_VALUE) + MIN_VALUE;
@@ -33,6 +37,8 @@ class Game {
   }
   void restartGame(){
     _targetValue = Random().nextInt(MAX_VALUE + 1 - MIN_VALUE) + MIN_VALUE;
+    //Guardamos la marca en la lista de marcas al reiniciar el juego.
+    _marks.add([_score, _rounds]);
     _points = 0;
     _score = 0;
     _rounds = 0;
